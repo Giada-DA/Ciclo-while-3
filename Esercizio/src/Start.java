2a5b527ebd8b57aa72f95bcf2b43c4b5021068b5
Giada Dall'Ara <giada.25700@gmail.com>
2023-02-12 18:24:21 +0100

    Esercizio corretto

--- Esercizio/src/Start.java
+++ Esercizio/src/Start.java
@@ -1,13 +1,18 @@
+
 public class Start {
     public static void main(String[] args) {
-        int[] fibonacci = new int[]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144};
+        int[] fibonacci = new int[10];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
         int i = 0;
-        while (i< fibonacci.length){
-            int value = fibonacci[i];
-            System.out.println(value);
-            if(value == 34){
-                break;
-            }
+        while(i<fibonacci.length-2){
+            int a = fibonacci[i] + fibonacci[i +1];
+            fibonacci[i + 2] = a;
+            i++;
+        }
+        i = 0;
+        while (i<fibonacci.length){
+            System.out.println(fibonacci[i]);
             i++;
         }
     }
